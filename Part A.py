1dfc5ea93ee40b7d6b178fbccb6fbf6a61713da0
Daminabotg <B00161001@mytudublin.ie>
2025-02-11 23:38:40 +0000

    Completed Part A

---
+++ Part A.py
@@ -0,0 +1,13 @@
+#Daminabo Tom-George
+#10/02/25
+#Program to Print steps 1 to 9 to the screen
+
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Syep 6")
+
+def main():
+    printSteps()
+
+main()
---
+++ Part B.py
@@ -0,0 +1,23 @@
+#Daminabo Tom-George
+#10/02/25
+#Part B
+
+def printSteps(a,b):
+    for i in range(a,b):
+        print("Steps " + str(i))
+
+print("Steps 1")
+print("Steps 2")
+print("Steps 3")
+print("Steps 4")
+print("Steps 5")
+print("Steps 6")
+print("Steps 7")
+print("Steps 8")
+print("Steps 9")
+
+def main():
+    printSteps
+
+
+main()
